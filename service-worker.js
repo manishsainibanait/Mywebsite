1481713e54247f38beda80434254e482818aefec
manishsainibanait <54795946+manishsainibanait@users.noreply.github.com>
2025-11-21 13:43:43 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+/* ===========================================================
+     SERVICE WORKER for CACHE CLEAR + FRESH LOAD
+=========================================================== */
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
