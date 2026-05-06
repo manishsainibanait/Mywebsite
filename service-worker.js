355b31e657789bdb9ef80ac698f8d6e31236cd6c
manishsainibanait <54795946+manishsainibanait@users.noreply.github.com>
2026-05-06 09:38:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,23 @@
      SERVICE WORKER for CACHE CLEAR + FRESH LOAD
 =========================================================== */
 
+const CACHE_NAME = "saini-app-v1";
+
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      )
+    )
   );
   self.clients.claim();
 });
